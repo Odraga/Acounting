7c1806789ac66ffdf234152bd66627f7fdfe5aec
Gabriel Vargas <75455545+Odraga@users.noreply.github.com>
2023-10-06 10:43:48 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Sidebar from "./components/common/Sidebar";
+import { routes } from "./Routes/routes";
+import Home from "./components/Home/Home";
+
+const RouteWithSidebar = ({ component: Component, ...rest }) => {
+  const [show, setShow] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      {!show ? (
+        <>
+          <div className="d-inline-flex">
+            <Sidebar />
+            <div className="container-md container-lg mt-0 mb-0">
+              <Component />
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={routes.Home.path}
+          element={<RouteWithSidebar component={Home} />}
+        />
+        {/* <Route
+          path={routes.AboutUs.path}
+          element={<RouteWithSidebar element={} />}
+        /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
