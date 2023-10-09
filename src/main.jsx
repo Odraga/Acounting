e3e09584e4aa9af5a52bdebcc286a39144f68071
Gabriel Vargas <75455545+Odraga@users.noreply.github.com>
2023-10-09 16:18:31 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./css/styles.css";
+import "react-confirm-alert/src/react-confirm-alert.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
