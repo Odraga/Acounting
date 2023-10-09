c6890e7a1649812ef0cb370ec45c33e43cf787d0
Gabriel Vargas <75455545+Odraga@users.noreply.github.com>
2023-10-09 16:20:30 -0400

    Create Income.jsx

---
+++ src/pages/Income.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ListIncome from "../components/Income/ListIncome";
+import Modal from "../components/common/Modal";
+
+const Income = ({ show, toggle }) => {
+  return (
+    <>
+      <ListIncome />
+    </>
+  );
+};
+
+export default Income;
