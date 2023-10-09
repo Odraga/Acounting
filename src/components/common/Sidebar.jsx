1b27c81a71ddbd60df8cf9c2e009e4fa5513a478
Gabriel Vargas <75455545+Odraga@users.noreply.github.com>
2023-10-09 16:15:19 -0400

    Update Sidebar.jsx

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -2,26 +2,49 @@ import { routes } from "../../Routes/routes";
 
 const Sidebar = () => {
   return (
-    <header className="sidebar mt-2">
-      <div>
-        <a className="sidebar-brand" href="#">
-          GABRIEL DEV
-        </a>
-        <ul className="sidebar-list mt-2">
-          <li className="sidebar-item">
-            <a href={routes.Home.path}>Home</a>
-          </li>
-          <li className="sidebar-item">
-            <a href={routes.AboutUs.path}>About</a>
-          </li>
-          <li className="sidebar-item">
-            <a href={routes.AboutUs.path}>Proyects</a>
-          </li>
-          <li className="sidebar-item">
-            <a href={routes.AboutUs.path}>Contact</a>
-          </li>
-        </ul>
-      </div>
+    <header className="d-none position-absolute d-sm-inline-flex bgc-primary vh-limited px-2 rounded-5">
+      <ul className="sidebar-list ps-0 mt-1">
+        <div className="sidebar-brand mx-3 my-2 line-buttom">
+          <strong>ACCOUNTING</strong>
+        </div>
+
+        <li className="sidebar-item mb-1">
+          <a href={routes.Home.path} className="px-2">
+            Home
+          </a>
+        </li>
+        <li className="sidebar-item mb-1">
+          <a href={routes.BankAccounts.path} className="px-2">
+            Cuentas Bancarias
+          </a>
+        </li>
+
+        <li className="sidebar-item mb-1">
+          <a href={routes.Expenses.path} className="px-2">
+            Gastos
+          </a>
+        </li>
+        <li className="sidebar-item mb-1">
+          <a href={routes.BankAccounts.path} className="px-2">
+            Ahorros
+          </a>
+        </li>
+        <li className="sidebar-item mb-1">
+          <a href={routes.BankAccounts.path} className="px-2">
+            Cambio de Divisa
+          </a>
+        </li>
+        <li className="sidebar-item mb-1">
+          <a href={routes.Income.path} className="px-2">
+            Ingresos
+          </a>
+        </li>
+        <li className="sidebar-item mb-1">
+          <a href={routes.Configuration.path} className="px-2">
+            Configuracion
+          </a>
+        </li>
+      </ul>
     </header>
   );
 };
