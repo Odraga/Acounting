1960670f6beff4273be2552931f863bb54c0366b
Gabriel Vargas <75455545+Odraga@users.noreply.github.com>
2023-10-09 16:18:40 -0400

    Create BankAccounts.jsx

---
+++ src/pages/BankAccounts.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ListBankAccounts from "../components/BankAccounts/ListBankAccounts";
+
+const BankAccounts = () => {
+  return <ListBankAccounts />;
+};
+
+export default BankAccounts;
