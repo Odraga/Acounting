832ca3a5ac6d4afe7c227a4c0714a8d047842803
Gabriel Vargas <75455545+Odraga@users.noreply.github.com>
2023-10-21 00:24:51 -0400

    Update AddEditBank.jsx

--- src/components/Configuration/Options/Banks/AddEditBank.jsx
+++ src/components/Configuration/Options/Banks/AddEditBank.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import ReactModal from "react-modal";
 import PropTypes from "prop-types";
 import DB from "../../../../common/ConsultingDB";
+import Modal from "../../../common/Modal";
 
 const AddEditBank = ({ show, toggle, model }) => {
   const [name, setName] = useState(model ? model.name : "");
@@ -30,28 +30,37 @@ const AddEditBank = ({ show, toggle, model }) => {
   };
 
   return (
-    <ReactModal
-      isOpen={show}
-      /*  onAfterOpen={afterOpenModal} */
-      onRequestClose={toggle}
-      /* style={customStyles} */
-      contentLabel="Example Modal"
-      ariaHideApp={false}
-    >
-      <button onClick={toggle}>Close</button>
-      <h2>ADD/EDIT BANKS</h2>
-      Name:
-      <br />
-      <input
-        type="text"
-        value={name}
-        onChange={(ev) => setName(ev.target.value)}
-      />
-      <br />
-      {!name ? <span>Por favor, escriba un nombre!</span> : null}
-      <br />
-      {name ? <button onClick={save}>Add</button> : null}
-    </ReactModal>
+    <Modal show={show} onHide={toggle} className={"z-100"}>
+      <Modal.Header onButtonClose>
+        <Modal.Title>ADD/EDIT BANK</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body className={"align-items-center justify-content-center"}>
+        <div>
+          Name:
+          <br />
+          <input
+            type="text"
+            value={name}
+            onChange={(ev) => setName(ev.target.value)}
+          />
+          <br />
+          {!name ? <span>Por favor, escriba un nombre!</span> : null}
+          <br />
+        </div>
+      </Modal.Body>
+      <Modal.Footer className={"justify-content-end align-items-center"}>
+        <div className="me-4">
+          {model ? (
+            name ? (
+              <button onClick={save}>Update</button>
+            ) : null
+          ) : name ? (
+            <button onClick={save}>Add</button>
+          ) : null}
+        </div>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
